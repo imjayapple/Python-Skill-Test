7b9674ce1587560b0ae1fb6a4b712b4f5967f33a
imjayapple <imjayapple@gmail.com>
2023-04-21 20:59:02 -0700

    Initial commit, 5 questions

---
+++ Skill-Test-1.py
@@ -0,0 +1,16 @@
+#1. What is the difference between a list and a tuple in Python?
+
+#2. What is the purpose of the __init__ method in a Python class?
+
+#3. How do you add an item to a set in Python?
+
+#4. What is the output of the following code?
+x = 10
+def foo():
+    x = 5
+    print(x)
+
+foo()
+print(x)
+
+#5. Write a simple function that takes a string as an input and returns the string reversed.
