d1420b00315128e23c39bcd983e6d43bf78744a7
imjayapple <imjayapple@gmail.com>
2023-04-23 22:17:29 -0700

    5 additional Python questions

--- Skill-Test-1.py
+++ Skill-Test-1.py
@@ -24,3 +24,13 @@ print(x)
 #9. What is list comprehension, and provide an example of how to use it.
 
 #10. Describe the difference between a shallow copy and a deep copy in Python.
+
+#11. Explain the concept of "duck typing" in Python and provide an example.
+
+#12. What are *args and **kwargs in Python, and how are they used in function arguments?
+
+#13. What is the purpose of the enumerate function, and provide an example of how to use it.
+
+#14. Describe the difference between a generator and a list in Python, and provide an example of using a generator.
+
+#15. What is a context manager in Python, and how is it typically used? Provide an example using the with statement.
